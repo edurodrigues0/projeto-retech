8e97f7a9d8e7efe405099b63acb06e30ce1e68bf
Eduardo Rodrigues <edusacramento9@gmail.com>
2023-12-14 23:59:59 +0000

    Responsive style in Success page

--- src/pages/Success.tsx
+++ src/pages/Success.tsx
@@ -1,34 +1,34 @@
-import { Button } from '@/components/ui/button'
-import successSVG from '../../public/success.svg'
-import { useNavigate, useParams } from 'react-router-dom'
-import { useEffect, useState } from 'react'
-import axios from 'axios'
+import { Button } from "@/components/ui/button";
+import successSVG from "../../public/success.svg";
+import { useNavigate, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 type Donor = {
   0: {
-    ID: string
-    Nome: string
-    Componente: string
-  }
-}
+    ID: string;
+    Nome: string;
+    Componente: string;
+  };
+};
 
 export function Success() {
-  const { requestId } = useParams()
-  const [donor, setDonor] = useState<Donor>()
-  const navigate = useNavigate()
+  const { requestId } = useParams();
+  const [donor, setDonor] = useState<Donor>();
+  const navigate = useNavigate();
 
   useEffect(() => {
     axios
       .get(`${import.meta.env.VITE_SHEET_API_URL}/query?ID=${requestId}`)
-      .then((response) => setDonor(response.data))
-  }, [requestId])
+      .then((response) => setDonor(response.data));
+  }, [requestId]);
 
   if (!donor) {
-    return
+    return;
   }
 
   return (
-    <div className="w-screen p-5 flex items-center justify-evenly gap-24 max-sm:flex-col-reverse max-sm:justify-center max-sm:items-center max-sm:gap-16">
+    <div className="w-screen h-full p-5 flex items-center justify-evenly gap-24 max-lg:flex-col-reverse max-sm:justify-center max-sm:items-center max-sm:gap-16">
       <div className="flex flex-col gap-5 w-[50rem] max-sm:w-full max-sm:gap-2">
         <h1 className="text-2xl font-bold leading-10 max-sm:text-lg">
           Projeto Re-Tech Agradece
@@ -40,10 +40,10 @@ export function Success() {
           peças de computadores que você gentilmente ofereceu. Sua contribuição
           é verdadeiramente inestimável e fará uma diferença significativa na
           experiência educacional dos nossos alunos. A generosidade que você
-          demonstrou ao compartilhar{' '}
+          demonstrou ao compartilhar{" "}
           <span className="font-bold">
             {donor[0].Componente.toLocaleLowerCase()}
-          </span>{' '}
+          </span>{" "}
           não apenas enriquecerá nosso laboratório de informática, mas também
           proporcionará aos alunos acesso a recursos tecnológicos essenciais.
           Seu gesto solidário não só fortalece a infraestrutura da instituição,
@@ -65,7 +65,7 @@ export function Success() {
         </span>
 
         <Button
-          onClick={() => navigate('/')}
+          onClick={() => navigate("/")}
           className="w-32 font-bold text-lg max-sm:w-full max-sm:text-sm max-sm:font-normal"
         >
           Voltar :D
@@ -73,10 +73,10 @@ export function Success() {
       </div>
 
       <img
-        className="w-96 max-sm:w-44 max-sm:mt-16"
+        className="w-96 max-lg:w-64 max-sm:w-44 max-sm:mt-16"
         src={successSVG}
         alt="Menina segurando um balão"
       />
     </div>
-  )
+  );
 }
